cde514b540d5fcedd05b0974e5904d2c8670887d
MarvinMichel <mmhsernee@gmail.com>
2020-10-21 16:59:35 +0200

    Add enzyme and simple tests

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,12 +3,15 @@ import { connect } from 'react-redux';
 import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses';
 
-const ExpenseList = props => (
+export const ExpenseList = props => (
   <div>
-    <h1>Expense list</h1>
-    {props.expenses.map(expense => {
-      return <ExpenseListItem key={expense.id} {...expense} />;
-    })}
+    {props.expenses.length === 0 ? (
+      <p>No expenses</p>
+    ) : (
+      props.expenses.map(expense => {
+        return <ExpenseListItem key={expense.id} {...expense} />;
+      })
+    )}
   </div>
 );
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -10,9 +10,6 @@ const Header = () => (
     <NavLink to="/create" activeClassName="is-active">
       Create Expense
     </NavLink>
-    <NavLink to="/help" activeClassName="is-active">
-      Help
-    </NavLink>
   </header>
 );
 
