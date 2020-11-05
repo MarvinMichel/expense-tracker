5833a2e7e1b96b00dc442508ed85aa405eeef208
MarvinMichel <mmhsernee@gmail.com>
2020-11-05 20:21:53 +0100

    Setup production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,52 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        resolve: {
-          extensions: ['.js', '.jsx'],
+module.exports = env => {
+  const isProduction = env === 'production';
+  const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
+    },
+    mode: 'development',
+    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          resolve: {
+            extensions: ['.js', '.jsx'],
+          },
         },
-      },
-      {
-        test: /\.s?css$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
+        {
+          test: /\.s?css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: (resourcePath, context) => {
+                  return path.relative(path.dirname(resourcePath), context) + '/';
+                },
+              },
+            },
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'styles.css',
+      }),
     ],
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true,
-  },
+  };
 };
